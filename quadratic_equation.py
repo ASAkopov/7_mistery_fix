b9feb51bcc0e03a961e4083e7e0802d6a5fc275f
ASAkopov <asakopov@gmail.com>
2018-04-08 23:23:15 +0300

    reduce comments

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,18 +2,13 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    """Solves quadratic equation a*x^2 + b*x + c = 0.
-    Inputs are coeffitients a, b, c and non-null a is
-    assumed. Depending on discriminant, returns two float,
-    one float or None, or both of None type roots.
-    """        
     discriminant = b ** 2 - 4 * a * c
     if a == 0:
         print("Function solves STRICTLY quadratic equation.")
         return
     if discriminant < 0:
         root1 = None
-        root2 = None        
+        root2 = None
     else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = None if discriminant == 0 else (-b + sqrt(discriminant)) / (2 * a)
