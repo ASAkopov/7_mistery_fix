5fad6e6877cb08187c11ef3dcb56f46659c0e3cc
ASAkopov <asakopov@gmail.com>
2018-04-09 18:43:19 +0300

    reduce nest level

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -5,8 +5,6 @@ def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant < 0:
         return None, None
-    else:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = None if discriminant == 0 \
-                else (-b + sqrt(discriminant)) / (2 * a)
-        return root1, root2
+    root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root2 = None if discriminant == 0 else (-b + sqrt(discriminant)) / (2 * a)
+    return root1, root2
