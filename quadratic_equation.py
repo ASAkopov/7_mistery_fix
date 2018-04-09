a68b845f5935eb885ac5bb5e38cbd70dec39448f
ASAkopov <asakopov@gmail.com>
2018-04-09 10:09:32 +0300

    correct long lines, avoids printing in function, make function code simplier

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,13 +3,10 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if a == 0:
-        print("Function solves STRICTLY quadratic equation.")
-        return
     if discriminant < 0:
-        root1 = None
-        root2 = None
+        return None, None
     else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = None if discriminant == 0 else (-b + sqrt(discriminant)) / (2 * a)
-    return root1, root2
+        root2 = None if discriminant == 0 \
+                else (-b + sqrt(discriminant)) / (2 * a)
+        return root1, root2
